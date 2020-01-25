d37ed6ed2454d05b070cc406eb4aaf09a17f9a44
8bitjustin <j.olson.digital@gmail.com>
2020-01-25 11:42:47 -0700

    Added 1.3 file.

---
+++ Chapter One/1_3 Infinite_Skills.py
@@ -0,0 +1,8 @@
+"""
+Three programming ideas:
+
+1. Currently I don't have any 'programming ideas', but I want to become fluent with much of what
+python offers. I need to know more of what I CAN do, so I can then determine the things I want to build.
+2.
+3.
+"""
