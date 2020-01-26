55f19f75dd6541d1cc7ff6276c47741d25b83d5d
8bitjustin <j.olson.digital@gmail.com>
2020-01-26 16:14:49 -0700

    Created apostrophe.py file.

---
+++ Chapter Two/2_apostrophe.py
@@ -0,0 +1,7 @@
+# will return correctly
+message = "One of Python's strengths is its diverse community"
+print(message)
+
+#will throw syntax error
+message = 'One of Python's strengths is its diverse community'
+print(message)
