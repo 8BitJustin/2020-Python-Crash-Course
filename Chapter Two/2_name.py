0e75e4901d4684d2db611aa47e717af6046b5c4a
8bitjustin <j.olson.digital@gmail.com>
2020-01-26 15:58:31 -0700

    Created 2_name.py file.

---
+++ Chapter Two/2_name.py
@@ -0,0 +1,15 @@
+name = "ada lovelace"
+print("1. " + name.title())
+
+name = "Ada Lovelace"
+print("2. " + name.upper())
+print("2. " + name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print("3. " + full_name)
+print("4. " + "Hello, " + full_name.title() + "!")
+
+message = "Hello, " + full_name.title() + "!"
+print("5. " + message)
