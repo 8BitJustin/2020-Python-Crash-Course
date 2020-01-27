47faebac822dd4c45610847a3fcd12eb210d98a8
8bitjustin <j.olson.digital@gmail.com>
2020-01-26 21:41:44 -0700

    Updated file name format for all files thus far. Created three more 'try yourself' challenges.

---
+++ Chapter Two/2-4 Name_Cases.py
@@ -0,0 +1,5 @@
+name = "justin"
+
+print(name.upper())
+print(name.lower())
+print(name.title())
---
+++ Chapter Two/2-5 Famous_Quote.py
@@ -0,0 +1,1 @@
+print('Albert Einstein once said, "Strive to not be of success, but to be of value."')
