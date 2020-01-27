7aedfc4b2b665975c167c339bf7e390cd6ccafed
8bitjustin <j.olson.digital@gmail.com>
2020-01-27 07:41:16 -0700

    Added last of Chapter 2 exercises.

---
+++ Chapter Two/2-11 Zen_of_Python.py
@@ -0,0 +1,3 @@
+"""
+Enter 'import this' into a python console or python terminal session and read through principles.
+"""
--- Chapter Two/2.9 Favorite_Number.py
+++ Chapter Two/2.9 Favorite_Number.py
@@ -1,3 +1,13 @@
+# Comment per 2-10 instructions, this is the morning of January 27th, 2020.
+"""
+Here
+is
+a
+multi
+line
+comment.
+"""
+
 fav_num = 34
 message = "My favorite number is, " + str(fav_num) + "."
 
