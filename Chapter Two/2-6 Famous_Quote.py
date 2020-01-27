74a075e01a7fdb054b80dc594658126017dce097
8bitjustin <j.olson.digital@gmail.com>
2020-01-26 21:50:41 -0700

    Added 2-6 and 2-7 exercises.

---
+++ Chapter Two/2-6 Famous_Quote.py
@@ -0,0 +1,4 @@
+famous_person = "Albert Einstein"
+message = "Strive to not be of success, but to be of value."
+
+print(famous_person + " once said, " + message)
---
+++ Chapter Two/2-7 Stripping_Names.py
@@ -0,0 +1,6 @@
+name = " justin "
+
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
