5c318018e277b67565aaa232a921dcde7cc01581
8bitjustin <j.olson.digital@gmail.com>
2020-01-29 08:03:33 -0700

    Added 3-4.

---
+++ Chapter Three/3-4 Guest_List.py
@@ -0,0 +1,5 @@
+guest_list = ["Jack Bauer", "Jack Ryan", "Jack Shit"]
+
+print("Welcome to the party, " + guest_list[0])
+print("Welcome to the party, " + guest_list[1])
+print("Welcome to the party, " + guest_list[2])
