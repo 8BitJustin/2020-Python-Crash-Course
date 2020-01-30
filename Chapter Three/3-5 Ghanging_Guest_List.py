edabdb7e50be80d0727397d401d73b3c12a5d67d
8bitjustin <j.olson.digital@gmail.com>
2020-01-29 21:03:58 -0700

    Added 3-5.

---
+++ Chapter Three/3-5 Ghanging_Guest_List.py
@@ -0,0 +1,12 @@
+guest_list = ["Jack Bauer", "Jack Ryan", "Jack Shit"]
+
+print(guest_list[0] + " is going to the party.")
+print(guest_list[1] + " is going to the party.")
+print(guest_list[2] + " is not going to the party.")
+print("\n")
+
+guest_list[2] = "Jack Skellington"
+
+print(guest_list[0] + " is going to the party.")
+print(guest_list[1] + " is going to the party.")
+print(guest_list[2] + " is going to the party.")
