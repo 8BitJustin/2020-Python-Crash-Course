cdc65e99fb4ae6b550f4f51ac5bd138808c595ed
8bitjustin <j.olson.digital@gmail.com>
2020-01-30 19:26:30 -0700

    Completed 3-8.

--- Chapter Three/3-8 Seeing_The_World.py
+++ Chapter Three/3-8 Seeing_The_World.py
@@ -9,3 +9,19 @@ print(world)
 print(sorted(world, reverse=True))
 
 print(world)
+
+world.reverse()
+
+print(world)
+
+world.reverse()
+
+print(world)
+
+world.sort()
+
+print(world)
+
+world.sort(reverse=True)
+
+print(world)
