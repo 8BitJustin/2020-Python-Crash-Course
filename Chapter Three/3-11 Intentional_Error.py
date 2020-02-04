461ce488887515eea0cdf8a43af68b0829718b74
8bitjustin <j.olson.digital@gmail.com>
2020-02-04 15:42:56 -0700

    Adjusted comment on 3-11.

--- Chapter Three/3-11 Intentional_Error.py
+++ Chapter Three/3-11 Intentional_Error.py
@@ -1,5 +1,5 @@
 cars_parts = ["exhaust", "grille", "body kit", "wrap", "hella horns"]
 
-print(cars_parts[5])  #returns an error
+print(cars_parts[5])  #returns an error due to this asking for the sixth item, only five present
 
 print(cars_parts[-1])  #will always print out last item
