55be0121bb5d59e407fd08fb65ef55def46f9fde
8bitjustin <j.olson.digital@gmail.com>
2020-02-09 12:43:31 -0700

    Completed 4.10.

--- Chapter 4/4-10 Slices.py
+++ Chapter 4/4-10 Slices.py
@@ -2,3 +2,9 @@ cars = ["WRX", "STI", "Type RA", "Series.White", "S209"]
 
 print("The first three items of the list are:")
 print(cars[0:3])
+
+print("The middle three items of the list are:")
+print(cars[1:4])
+
+print("The last three items of the list are:")
+print(cars[-3:])
