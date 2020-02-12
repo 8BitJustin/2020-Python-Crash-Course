78007159ac71c75291896762d29c3801fb935b68
8bitjustin <j.olson.digital@gmail.com>
2020-02-12 07:38:02 -0700

    Completed 4.13.

---
+++ Chapter 4/4-13 Buffet.py
@@ -0,0 +1,14 @@
+buffet = ('chicken', 'beef', 'pork', 'beans', 'rice')
+
+print("Original List")
+for j in buffet:
+    print(j)
+
+# buffet[0] = 'cupcake'
+print("\n")
+
+buffet = ('chicken', 'lamb', 'pork', 'fajita', 'rice')
+
+print("Updated List")
+for j in buffet:
+    print(j)
