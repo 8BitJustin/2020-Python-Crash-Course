ffea35acbf899454f06e422cbc28ae8f37cc911b
8bitjustin <j.olson.digital@gmail.com>
2020-02-17 13:54:32 -0700

    Completed 5.7.

---
+++ Chapter 5/5-7 Favorite_Fruit.py
@@ -0,0 +1,13 @@
+favorite_fruits = ["strawberries", "tangerines", "grapes"]
+
+if "strawberries" in favorite_fruits:
+    print("I really love strawberries!")
+
+if "bananas" in favorite_fruits:
+    print("Not on the list!")
+
+if "tangerines" in favorite_fruits:
+    print("I really love tangerines!")
+
+if "grapes" in favorite_fruits:
+    print("I really love grapes!")
