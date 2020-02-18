579f015892526299929041c6ddc6b497a82bac3c
8bitjustin <j.olson.digital@gmail.com>
2020-02-18 09:02:21 -0700

    Created and completed 5.11.

---
+++ Chapter 5/5-11 Ordinal_Numbers.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for j in numbers:
+    if j == 1:
+        print(str(j) + 'st')
+    elif j == 2:
+        print(str(j) + 'nd')
+    elif j == 3:
+        print(str(j) + 'rd')
+    else:
+        print(str(j) + 'th')
