0e6dbcb8c977707bffca31f5ae27bc1bd069943d
8bitjustin <j.olson.digital@gmail.com>
2020-02-19 21:22:04 -0700

    Completed 6.1.

---
+++ Chapter 6/6-1 Person.py
@@ -0,0 +1,11 @@
+person = {
+    'first_name': 'Justin',
+    'last_name': 'Olson',
+    'age': 39,
+    'city': 'Tucson'
+}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
