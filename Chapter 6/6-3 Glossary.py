38928e86ba7137fb0d6ca03e0f2bc92f162fbdfd
8bitjustin <j.olson.digital@gmail.com>
2020-02-20 21:35:36 -0700

    Completed 6.3.

---
+++ Chapter 6/6-3 Glossary.py
@@ -0,0 +1,11 @@
+languages = {
+    'python': 1991,
+    'ruby': 1995,
+    'javascript': 1995,
+    'c#:': 1995,
+    'kotlin': 2011
+}
+
+for k, v in languages.items():
+    print('\nLanguage: ' + k.title())
+    print('\tCreated: ' + str(v))
