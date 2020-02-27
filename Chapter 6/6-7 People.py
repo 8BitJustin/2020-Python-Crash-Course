a4c6f031cae6ecc15a024a57b2a806e2bf20a41a
8bitjustin <j.olson.digital@gmail.com>
2020-02-26 20:57:14 -0700

    Completed 6.7.

---
+++ Chapter 6/6-7 People.py
@@ -0,0 +1,25 @@
+person_1 = {
+    'first_name': 'James',
+    'last_name': 'Olson',
+    'age': 37,
+    'state': 'Arizona'
+}
+
+person_2 = {
+    'first_name': 'Justin',
+    'last_name': 'Olson',
+    'age': 39,
+    'state': 'Arizona'
+}
+
+person_3 = {
+    'first_name': 'Nick',
+    'last_name': 'Olson',
+    'age': 41,
+    'state': 'Alabama'
+}
+
+people = [person_1, person_2, person_3]
+
+for j in people:
+    print(j)
