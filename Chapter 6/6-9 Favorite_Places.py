f0df9bd078389ff68881907c8ce8390c6a6ee385
8bitjustin <j.olson.digital@gmail.com>
2020-02-26 21:12:30 -0700

    Completed 6.9.

--- Chapter 6/6-9 Favorite_Places.py
+++ Chapter 6/6-9 Favorite_Places.py
@@ -1,3 +1,10 @@
 favorite_places = {
-
+    'justin': ['norway', 'japan', 'italy'],
+    'lynette': ['germany'],
+    'skyler': ['italy', 'germany']
 }
+
+for names, locs in favorite_places.items():
+    print('\n' + names.title() + ' would like to visit:')
+    for loc in locs:
+        print('\t' + loc.title())
