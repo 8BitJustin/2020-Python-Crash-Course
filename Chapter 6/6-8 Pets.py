143337c7696760c97f133200485384b06339aa9b
8bitjustin <j.olson.digital@gmail.com>
2020-02-26 20:59:39 -0700

    Completed 6.8.

---
+++ Chapter 6/6-8 Pets.py
@@ -0,0 +1,22 @@
+pet_1 = {
+    'name': 'sirena',
+    'owner': 'justin',
+    'species': 'cat'
+}
+
+pet_2 = {
+    'name': 'sammie',
+    'owner': 'justin',
+    'species': 'cat'
+}
+
+pet_3 = {
+    'name': 'phoenix',
+    'owner': 'lynette',
+    'species': 'dog'
+}
+
+pets = [pet_1, pet_2, pet_3]
+
+for p in pets:
+    print(p)
