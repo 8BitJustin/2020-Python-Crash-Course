21b7280e56d1f0b2d35af5577d5443740d2e7010
8bitjustin <j.olson.digital@gmail.com>
2020-03-06 15:48:48 -0700

    Created and completed 8.4.

---
+++ Chapter 8 - Functions/8-4 Large_Shirts.py
@@ -0,0 +1,8 @@
+def make_shirt(size='large', text='I love Python!'):
+    print('You ordered a size ' + size + ' t-shirt with the text \"' + text +
+          '\" on it.')
+
+
+make_shirt()
+make_shirt('medium')
+make_shirt('small', 'Boosted Syndicate')
