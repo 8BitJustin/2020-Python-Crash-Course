8a53bf41cadebc77efe211ceb988e5953c10f6d2
8bitjustin <j.olson.digital@gmail.com>
2020-03-06 07:04:52 -0700

    Completed 7.8.

--- Chapter 7/7-8 Deli.py
+++ Chapter 7/7-8 Deli.py
@@ -2,3 +2,11 @@ sandwich_orders = ['italian', 'pastrami', 'salami', 'ham']
 
 finished_sandwiches = []
 
+while sandwich_orders:
+    building = sandwich_orders.pop()
+    print(building.title() + ' sandwich being built...')
+    finished_sandwiches.append(building)
+
+print('\n***')
+for finished in finished_sandwiches:
+    print(finished.title() + ' completed.')
