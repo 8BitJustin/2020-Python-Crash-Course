d176de4a3b24d4695c4f3bd2113ef6749af2104f
8bitjustin <j.olson.digital@gmail.com>
2020-03-06 15:57:04 -0700

    Created and completed 8.5.

---
+++ Chapter 8 - Functions/8-5 Cities.py
@@ -0,0 +1,7 @@
+def describe_city(city, country='United States'):
+    print('The city of ' + city + ' is in ' + country + '.')
+
+
+describe_city('Tucson')
+describe_city('Phoenix')
+describe_city('Rome', 'Italy')
