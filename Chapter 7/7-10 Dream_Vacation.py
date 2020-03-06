aac6fa607d5e74323b998c2a75ab97e2a46929e8
8bitjustin <j.olson.digital@gmail.com>
2020-03-06 07:53:31 -0700

    Updated 7.10 with comments.

--- Chapter 7/7-10 Dream_Vacation.py
+++ Chapter 7/7-10 Dream_Vacation.py
@@ -1,16 +1,19 @@
+# Stores replies from input within while loop
 replies = {}
 
 polling = True
 
+# will run as long as polling is True
 while polling:
     name = input("What is your name? ")
     visit = input("What place would you like to visit? ")
 
+    # Places 'name' as the Key and 'visit' as that key's 'value'
     replies[name] = visit
-    replies['omg'] = 'testing!!'
 
     again = input("Would someone else like to try? (y/n) ")
     if again == 'n':
+        # Sets polling to False, thus killing the loop
         polling = False
 
 print('\n---===*** Results ***===---')
