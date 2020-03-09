41aa16e9114aee372c74e1bb2be91debb19dad0b
8bitjustin <j.olson.digital@gmail.com>
2020-03-08 18:17:55 -0700

    Completed 8.9.

---
+++ Chapter 8 - Functions/8-9 Magicians.py
@@ -0,0 +1,8 @@
+magicians = ['houdini', 'copperfield', 'blaine']
+
+def show_magicians(names):
+    for name in names:
+        print('Hello, Mr. ' + name.title() + '.')
+
+
+show_magicians(magicians)
