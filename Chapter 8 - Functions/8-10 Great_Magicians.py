2b961f61bc72a680dc2d9038ae713eadbd73de37
8bitjustin <j.olson.digital@gmail.com>
2020-03-09 08:09:21 -0700

    Completed 8.10.

---
+++ Chapter 8 - Functions/8-10 Great_Magicians.py
@@ -0,0 +1,17 @@
+def make_great(not_yet_great, now_great):
+    while not_yet_great:
+        popping = not_yet_great.pop()
+        popping = popping + ', the Great'
+        now_great.append(popping)
+
+
+def show_magicians(names):
+    for name in names:
+        print(name.title() + '.')
+
+
+magicians = ['houdini', 'copperfield', 'blaine']
+greats = []
+
+make_great(magicians, greats)
+show_magicians(greats)
