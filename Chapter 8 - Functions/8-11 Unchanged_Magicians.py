be0a72bfcbd610c95e77b5e492e288321d25dbbb
8bitjustin <j.olson.digital@gmail.com>
2020-03-09 10:49:16 -0700

    Added reverse method to make_great function within 8.11.

--- Chapter 8 - Functions/8-11 Unchanged_Magicians.py
+++ Chapter 8 - Functions/8-11 Unchanged_Magicians.py
@@ -3,6 +3,9 @@ def make_great(not_yet_great, now_great):
         popping = not_yet_great.pop()
         popping = popping + ', the Great'
         now_great.append(popping)
+    # added .reverse to now_great as to keep it in order, as using pop will
+    # change the order of the list, going from last to first
+    now_great.reverse()
 
 
 def show_magicians(names):
