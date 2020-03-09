2247502286f6cb345bef471516cc9b124449ed1f
8bitjustin <j.olson.digital@gmail.com>
2020-03-09 13:20:06 -0700

    Added to 8.12.

--- Chapter 8 - Functions/8-12 Sandwiches.py
+++ Chapter 8 - Functions/8-12 Sandwiches.py
@@ -1,6 +1,7 @@
 def sammich_toppings(*toppings):
     print('To be added to sammich...')
-    print(toppings)
+    for topping in toppings:
+        print('- ' + topping)
 
 
 sammich_toppings('salami', 'pepperjack', 'spicy mustard')
