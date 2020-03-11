cfd9f661f6b642c93ca43bcabdeb4dc599065b90
8bitjustin <j.olson.digital@gmail.com>
2020-03-11 12:05:42 -0700

    Reworked 8.14, reqrote car dictionary as dictionary literal, and renamed arbitrary argument to added.

--- Chapter 8 - Functions/8-14 Cars.py
+++ Chapter 8 - Functions/8-14 Cars.py
@@ -1,10 +1,9 @@
-def build_car(make, model, **addl):
-    car = {}
-    car['make'] = make
-    car['model'] = model
-    for k,v in addl.items():
+def build_car(make, model, **added):
+    car = {'make': make, 'model': model}
+    for k, v in added.items():
         car[k] = v
     return car
+
 
 subaru = build_car('subaru', 'wrx', color='silver')
 
