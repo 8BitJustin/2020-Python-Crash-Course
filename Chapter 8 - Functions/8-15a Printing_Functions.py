c2e4850e45c49726c2ec19f251c5e19bd1ced467
8bitjustin <j.olson.digital@gmail.com>
2020-03-12 21:07:20 -0700

    Altered both 8-15 files.

---
+++ Chapter 8 - Functions/8-15a Printing_Functions.py
@@ -0,0 +1,7 @@
+import printing_functions
+
+magicians = ['houdini', 'copperfield', 'blaine']
+greats = []
+
+printing_functions.make_great(magicians, greats)
+printing_functions.show_magicians(greats)
