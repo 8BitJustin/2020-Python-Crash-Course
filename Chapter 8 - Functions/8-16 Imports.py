358052a611d6331bbce3b1f363dcae9013924daf
8bitjustin <j.olson.digital@gmail.com>
2020-03-13 07:45:21 -0700

    Added comments to 8.16.

--- Chapter 8 - Functions/8-16 Imports.py
+++ Chapter 8 - Functions/8-16 Imports.py
@@ -1,8 +1,17 @@
+from create_car import build_car as bc
+
 """
 Using with create_car.py
 """
 
-from create_car import build_car as bc
+"""
+Other ways to import:
+import module_name
+from module_name import function_name
+from module_name import function_name as fn
+import module_name as mn
+from module_name import *
+"""
 
 subaru = bc('subaru', 'wrx', color='silver', completed=10)
 
