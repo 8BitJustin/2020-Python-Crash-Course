c3d6d0b00185c37102f0c8792d5a997d5a9892b4
8bitjustin <j.olson.digital@gmail.com>
2020-03-18 18:21:16 -0700

    Completed 9.5.

--- Chapter 9 - Classes/9-5 Login_Attempts.py
+++ Chapter 9 - Classes/9-5 Login_Attempts.py
@@ -2,9 +2,19 @@ class Users():
     def __init__(self, first_name, last_name):
         self.fname = first_name
         self.lname = last_name
+        self.login_attempts = 1
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+        return self.login_attempts
+
+    def reset_login_attempts(self):
+        self.login_attempts = 1
+        return self.login_attempts
 
     def describe_user(self):
-        user_name = 'User: ' + self.fname.title() + ' ' + self.lname.title()
+        user_name = 'User: ' + self.fname.title() + ' ' + self.lname.title()\
+                    + '\nLogin Attempts: ' + str(self.login_attempts)
         return user_name
 
     def greet_user(self):
@@ -12,3 +22,17 @@ class Users():
                    + '. '
         return greeting
 
+
+person = Users('justin', 'olson')
+
+person.increment_login_attempts()
+person.increment_login_attempts()
+person.increment_login_attempts()
+
+print(person.describe_user())
+
+print()
+
+person.reset_login_attempts()
+
+print(person.describe_user())
