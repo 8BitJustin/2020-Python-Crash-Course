fd448cf39fb3f643edb5a613febd13af22046ecd
8bitjustin <j.olson.digital@gmail.com>
2020-03-22 18:24:13 -0700

    Completed 9.6.

--- Chapter 9 - Classes/9-6 Ice_Cream_Stand.py
+++ Chapter 9 - Classes/9-6 Ice_Cream_Stand.py
@@ -9,3 +9,18 @@ class Restaurant():
 
     def open_restaurant(self):
         return self.name.title() + ' is open!'
+
+
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name, cuisine_type):
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ['vanilla', 'chocolate', 'strawberry']
+
+    def show_flavors(self):
+        message = 'We serve: '
+        for i in self.flavors:
+            print(message + i.title())
+
+
+ice_palace = IceCreamStand('NIICE', 'ice cream')
+print(ice_palace.show_flavors())
