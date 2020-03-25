7c2bd30ec0e28b8791556e0e04c07c344b6e2e5a
8bitjustin <j.olson.digital@gmail.com>
2020-03-24 21:16:24 -0700

    Working on 9.7.

---
+++ Chapter 9 - Classes/9-7 Admin.py
@@ -0,0 +1,23 @@
+class Users():
+    def __init__(self, first_name, last_name):
+        self.fname = first_name
+        self.lname = last_name
+        self.login_attempts = 1
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+        return self.login_attempts
+
+    def reset_login_attempts(self):
+        self.login_attempts = 1
+        return self.login_attempts
+
+    def describe_user(self):
+        user_name = 'User: ' + self.fname.title() + ' ' + self.lname.title()\
+                    + '\nLogin Attempts: ' + str(self.login_attempts)
+        return user_name
+
+    def greet_user(self):
+        greeting = 'Hello, ' + self.fname.title() + ' ' + self.lname.title()\
+                   + '. '
+        return greeting
