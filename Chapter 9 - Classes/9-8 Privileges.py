23dd911998cfd04d7d68abf81b9ce843711e03b4
8bitjustin <j.olson.digital@gmail.com>
2020-03-26 18:41:45 -0700

    Working on 9.8.

---
+++ Chapter 9 - Classes/9-8 Privileges.py
@@ -0,0 +1,38 @@
+class Users():
+    def __init__(self, first_name, last_name):
+        self.fname = first_name
+        self.lname = last_name
+        self.login_attempts = 1
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+        return self.login_attempts
+
+    def reset_login_attempts(self):
+        self.login_attempts = 1
+        return self.login_attempts
+
+    def describe_user(self):
+        user_name = 'User: ' + self.fname.title() + ' ' + self.lname.title()\
+                    + '\nLogin Attempts: ' + str(self.login_attempts)
+        return user_name
+
+    def greet_user(self):
+        greeting = 'Hello, ' + self.fname.title() + ' ' + self.lname.title()\
+                   + '. '
+        return greeting
+
+
+class Admin(Users):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.privileges = ['can add post', 'can delete post', 'can ban user']
+
+    def show_privileges(self):
+        print('You have the following privileges:')
+        for i in self.privileges:
+            print('\t' + i.title())
+
+
+superuser = Admin('justin', 'olson')
+superuser.show_privileges()
