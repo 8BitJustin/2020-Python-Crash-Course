c60d54a804f6572bbde9198777fe669cbeeaf86f
8bitjustin <j.olson.digital@gmail.com>
2020-03-31 07:41:09 -0700

    Added describe_admin to Admin class.

--- Chapter 9 - Classes/user.py
+++ Chapter 9 - Classes/user.py
@@ -1,4 +1,6 @@
+# Imported specific class (Admin) from user_module module
 from user_module import Admin
 
 superuser = Admin('justin', 'olson')
+superuser.describe_admin()
 superuser.privileges.show_privileges()
--- Chapter 9 - Classes/user_module.py
+++ Chapter 9 - Classes/user_module.py
@@ -26,7 +26,14 @@ class Users():
 class Admin(Users):
     def __init__(self, first_name, last_name):
         super().__init__(first_name, last_name)
+        self.first_name = first_name
+        self.last_name = last_name
         self.privileges = Privileges()
+
+    def describe_admin(self):
+        admin_name = self.first_name.title() + ' ' + self.last_name.title() +\
+                    ', you are now an admin.'
+        print(admin_name)
 
 
 class Privileges:
