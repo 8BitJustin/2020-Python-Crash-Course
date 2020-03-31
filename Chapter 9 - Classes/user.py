8cfda2b8e6b01532c60828ad3bd557020cf158cd
8bitjustin <j.olson.digital@gmail.com>
2020-03-31 07:56:39 -0700

    Added more comments to user.py.

--- Chapter 9 - Classes/user.py
+++ Chapter 9 - Classes/user.py
@@ -1,6 +1,23 @@
 # Imported specific class (Admin) from user_module module
 from user_module import Admin
 
+# Creates superuser variable using Admin class.
 superuser = Admin('justin', 'olson')
+
+# Uses the describe_admin method within Admin class.
 superuser.describe_admin()
+
+# Uses the 'self.privileges' within Admin to access the Privileges class,
+# then use the show_privileges method.
 superuser.privileges.show_privileges()
+
+"""
+From top to bottom:
+
+superuser variable was created using the imported Admin class. Then 
+superuser used the describe_admin() within the Admin class. Finally, 
+the superuser accessed the show_privileges() method within the Privileges 
+class by accessing self.privileges (privileges) within Admin class. This is 
+tied to Privileges(), which is a backdoor so-to-speak with accessing the 
+Privileges class, which wasn't actually imported into this file.
+"""
