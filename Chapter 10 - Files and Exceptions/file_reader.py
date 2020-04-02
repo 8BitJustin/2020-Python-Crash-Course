146c5bdb2a37e168412850bc0f5f85ee7963d072
8bitjustin <j.olson.digital@gmail.com>
2020-04-02 16:45:45 -0700

    updated file_reader.py.

--- Chapter 10 - Files and Exceptions/file_reader.py
+++ Chapter 10 - Files and Exceptions/file_reader.py
@@ -6,7 +6,20 @@
 
 filename = 'pi_digits.txt'
 
+# with open(filename) as file_object:
+#     for line in file_object:
+#         # rstrip removes blank spaces after last line finishes
+#         print(line.rstrip())
+
 with open(filename) as file_object:
-    for line in file_object:
-        # rstrip removes blank spaces after last line finishes
-        print(line.rstrip())
+    lines = file_object.readlines()
+#
+# for line in lines:
+#     print(line.rstrip())
+
+pi_string = ''
+for line in lines:
+    pi_string += line.strip()
+
+print(pi_string)
+print(len(pi_string))
