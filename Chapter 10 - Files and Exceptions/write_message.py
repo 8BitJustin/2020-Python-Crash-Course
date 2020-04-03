8a70e975b0b3944a0a22c6f4fa88093ce854b36b
8bitjustin <j.olson.digital@gmail.com>
2020-04-03 16:02:26 -0700

    Added code to write_message.py file that appends to programming.txt file.

--- Chapter 10 - Files and Exceptions/write_message.py
+++ Chapter 10 - Files and Exceptions/write_message.py
@@ -1,5 +1,13 @@
 filename = 'programming.txt'
 
 with open(filename, 'w') as file_object:
-    file_object.write('I love programming!')
+    # Creates programming.txt file, writes first line, goes to new line,
+    # writes following line.
+    file_object.write('I love programming!\n')
+    file_object.write('I love creating programs.\n')
 
+
+with open(filename, 'a') as file_object:
+    # This 'appends' text to the programming.txt file.
+    file_object.write('I also love finding meaning in large datasets.\n')
+    file_object.write('I love creating apps that can run in a browser.\n')
