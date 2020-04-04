b5212eae5896aa4396846ded3af5225a4da7cd7a
8bitjustin <j.olson.digital@gmail.com>
2020-04-04 11:01:33 -0700

    Corrected text within alice.txt file.

--- Chapter 10 - Files and Exceptions/alice.py
+++ Chapter 10 - Files and Exceptions/alice.py
@@ -1,8 +1,13 @@
 filename = 'alice.txt'
 
 try:
-    with open(filename) as f_obj:
-        contents = f_obj.read()
+    with open(filename) as file_object:
+        contents = file_object.read()
 except FileNotFoundError:
     message = f"Sorry, the file: {filename} does not exist."
     print(message)
+else:
+    # Counts the approximate number of words in given file.
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {filename} has about {str(num_words)} words.")
