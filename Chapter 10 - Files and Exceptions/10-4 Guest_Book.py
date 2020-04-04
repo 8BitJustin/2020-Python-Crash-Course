797f740bb83c4c71683fea19ec7bdfb3f819200b
8bitjustin <j.olson.digital@gmail.com>
2020-04-03 19:20:21 -0700

    Added comments to 10.4.

--- Chapter 10 - Files and Exceptions/10-4 Guest_Book.py
+++ Chapter 10 - Files and Exceptions/10-4 Guest_Book.py
@@ -1,6 +1,12 @@
 filename = 'guest_book.txt'
 
 while True:
+    """
+    Until program is stopped, input will keep asking for names to attend.
+    
+    When a name is given, it is appended to the guest_book.txt file, 
+    and a message welcoming them to the event is printed.
+    """
     with open('guest_book.txt', 'a') as file_object:
         guest_name = input("Please provide your name for the guest list: ")
         message = f"We are welcome to have you attend, {guest_name.title()}!"
