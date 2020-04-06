43f636de36d89427996a38cf56b5c85089d73b39
8bitjustin <j.olson.digital@gmail.com>
2020-04-06 08:08:24 -0700

    Updated code on number_writer.py. File now has dump and load code to demonstrate json usage in Python.

--- Chapter 10 - Files and Exceptions/number_writer.py
+++ Chapter 10 - Files and Exceptions/number_writer.py
@@ -2,6 +2,13 @@ import json
 
 numbers = [2, 3, 5, 7, 11, 13]
 
+# Dumps numbers list into numbers.json file.
 filename = 'numbers.json'
-with open(filename, 'w') as f_obj:
-    json.dump(numbers, f_obj)
+# with open(filename, 'w') as f_obj:
+#     json.dump(numbers, f_obj)
+
+# Loads numbers list from numbers.json file.
+with open(filename) as f_obj_1:
+    load_numbers = json.load(f_obj_1)
+
+print(load_numbers)
