0f1d71a013074ef59d8dc5c0bba8f0a3e678a0ca
8bitjustin <j.olson.digital@gmail.com>
2020-04-06 08:47:11 -0700

    Updated remember_me.py file so both remember_me.py code and greet_user.py code/files are on same file.

--- Chapter 10 - Files and Exceptions/remember_me.py
+++ Chapter 10 - Files and Exceptions/remember_me.py
@@ -1,9 +1,17 @@
 import json
 
-username = input("What is your name? ")
+# Load username if previously stored.
+# Otherwise prompt for username and store it.
 
 filename = 'username.json'
 
-with open(filename, 'w') as file_object:
-    json.dump(username, file_object)
-    print("We'll remember you when you come back!")
+try:
+    with open(filename) as file_object:
+        username = json.load(file_object)
+except FileNotFoundError:
+    username = input("What is your name? ")
+    with open(filename, 'w') as file_object:
+        json.dump(username, file_object)
+        print("We'll remember you when you come back!")
+else:
+    print(f"Welcome back {username}!")
