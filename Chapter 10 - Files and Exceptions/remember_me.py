39c41abdc2ea213d03a49e3c5b2a35ecfe444d2f
8bitjustin <j.olson.digital@gmail.com>
2020-04-07 08:27:27 -0700

    Remember_me.py file converted to two functions: get_stored_username() and greet_user().

--- Chapter 10 - Files and Exceptions/remember_me.py
+++ Chapter 10 - Files and Exceptions/remember_me.py
@@ -1,20 +1,31 @@
 import json
 
 
-def greet_user():
-    """Greet user by name."""
-    filename = 'username.json'
+def get_stored_username():
+    """Get stored username if available."""
 
+    filename = 'username.json'
     try:
         with open(filename) as file_object:
             username = json.load(file_object)
     except FileNotFoundError:
+        return None
+    else:
+        return username
+
+
+def greet_user():
+    """Greet user by name."""
+
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back {username.title()}!")
+    else:
         username = input("What is your name? ")
+        filename = 'username.json'
         with open(filename, 'w') as file_object:
             json.dump(username, file_object)
             print(f"We'll remember you when you come back, {username.title()}!")
-    else:
-        print(f"Welcome back {username.title()}!")
 
 
 greet_user()
