54170ceed37fb8863eb270ed3943722271152f16
8bitjustin <j.olson.digital@gmail.com>
2020-04-10 08:22:28 -0700

    Test_name_function.py created and completed.

--- Chapter 11 - Testing Code/test_name_function.py
+++ Chapter 11 - Testing Code/test_name_function.py
@@ -1,4 +1,14 @@
 import unittest
 from name_function import get_formatted_name
 
-class NamesTestCase(unittest.TestCase)
+
+class NamesTestCase(unittest.TestCase):
+    """Tests for 'name_function.py'."""
+
+    def test_first_last_name(self):
+        """Do names like 'Jackie Chan' work?"""
+        formatted_name = get_formatted_name('jackie', 'chan')
+        self.assertEqual(formatted_name, 'Jackie Chan')
+
+
+unittest.main()
