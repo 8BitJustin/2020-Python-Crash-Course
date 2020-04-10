0af3ce14fbe933e9c12f272e16978eabe32f5cda
8bitjustin <j.olson.digital@gmail.com>
2020-04-10 14:29:07 -0700

    Altered name_function (added middle parameter) to have test give off error.

--- Chapter 11 - Testing Code/name_function/__init__.py
+++ Chapter 11 - Testing Code/name_function/__init__.py
@@ -1,9 +1,11 @@
-def get_formatted_name(first, last):
+def get_formatted_name(first, middle, last):
     """
     Creates formatted name.
     :param first: First name.
+    :param middle: Middle name.
     :param last: Last name.
     :return: First and last name with capitalized first letters.
     """
-    full_name = f"{first.title()} {last.title()}"
+
+    full_name = f"{first.title()} {middle.title()} {last.title()}"
     return full_name
