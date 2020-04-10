6ea4227795b2b2767e560c11a9dc88029b71ecdb
8bitjustin <j.olson.digital@gmail.com>
2020-04-10 09:39:44 -0700

    Added docstring to name_function.

--- Chapter 11 - Testing Code/name_function/__init__.py
+++ Chapter 11 - Testing Code/name_function/__init__.py
@@ -1,3 +1,9 @@
 def get_formatted_name(first, last):
+    """
+    Creates formatted name.
+    :param first: First name.
+    :param last: Last name.
+    :return: First and last name with capitalized first letters.
+    """
     full_name = f"{first.title()} {last.title()}"
     return full_name
