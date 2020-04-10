bd3c6e1efe0048f4258bd765b1d47fb5b65af410
8bitjustin <j.olson.digital@gmail.com>
2020-04-10 09:38:13 -0700

    Completed names.py.

---
+++ Chapter 11 - Testing Code/names.py
@@ -0,0 +1,14 @@
+from name_function import get_formatted_name
+
+print("Enter 'q' at any time to quit.")
+
+while True:
+    first = input("\nFirst name? ")
+    if first == 'q':
+        break
+    last = input("Last name? ")
+    if last == 'q':
+        break
+
+    formatted_name = get_formatted_name('justin', 'olson')
+    print(f"\n\tFormatted name: {formatted_name}")
