20c705070018e83dde4b4908d199c0a2af4479e4
8bitjustin <j.olson.digital@gmail.com>
2020-04-12 14:36:22 -0700

    Adjusted city_country to show population properly.

--- Chapter 11 - Testing Code/city_country/__init__.py
+++ Chapter 11 - Testing Code/city_country/__init__.py
@@ -1,6 +1,10 @@
 def city_country(city, country, population):
     city_name = city
     country_name = country
-    area = f"{city_name.title()}, {country_name.title()} - {population}"
+    area = f"{city_name.title()}, {country_name.title()} - {str(population)}"
 
     return area
+
+
+# Test
+print(city_country('santiago', 'chile', 5000000))
