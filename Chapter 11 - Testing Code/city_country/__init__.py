7d63be8b2bcdb3ced656dccdec4034119f5d4702
8bitjustin <j.olson.digital@gmail.com>
2020-04-12 15:02:29 -0700

    Altered city_country to accept population properly.

--- Chapter 11 - Testing Code/city_country/__init__.py
+++ Chapter 11 - Testing Code/city_country/__init__.py
@@ -1,10 +1,14 @@
-def city_country(city, country, population):
+def city_country(city, country, population=""):
     city_name = city
     country_name = country
-    area = f"{city_name.title()}, {country_name.title()} - {str(population)}"
-
+    city_population = population
+    if city_population:
+        area = f"{city_name.title()}, {country_name.title()} - {str(population)}"
+    else:
+        area = f"{city_name.title()}, {country_name.title()}"
     return area
 
 
 # Test
 # print(city_country('santiago', 'chile', 5000000))
+# print(city_country('santiago', 'chile'))
