0f3d1951b580d9109e33383a7191119a18166684
8bitjustin <j.olson.digital@gmail.com>
2020-04-12 14:33:17 -0700

    Added population parameter to city_country module. Gives error to test.

--- Chapter 11 - Testing Code/city_country/__init__.py
+++ Chapter 11 - Testing Code/city_country/__init__.py
@@ -1,6 +1,6 @@
-def city_country(city, country):
+def city_country(city, country, population):
     city_name = city
     country_name = country
-    area = f"{city_name.title()}, {country_name.title()}"
+    area = f"{city_name.title()}, {country_name.title()} - {population}"
 
     return area
