655d600e61d2290a8b38817cb4533aa12efb6b2c
8bitjustin <j.olson.digital@gmail.com>
2020-04-14 15:27:45 -0700

    Test_employees.py file updated with setUp() method. Working properly.

--- Chapter 11 - Testing Code/test_employee.py
+++ Chapter 11 - Testing Code/test_employee.py
@@ -5,12 +5,13 @@ from employee import CreateEmployee
 class TestCreateEmployee(unittest.TestCase):
     """Tests for CreateEmployee class."""
 
+    def setUp(self):
+        self.new_employee = CreateEmployee('j-bone', 'saturno', 50000)
+        self.default_raise_employee = self.new_employee.description()
+        self.custom_raise_employee = self.new_employee.give_raise(5000)
+
     def test_give_default_raise(self):
-        new_employee = CreateEmployee('j-bone', 'saturno', 50000)
-        default_raise_employee = new_employee.description()
-        self.assertEqual(default_raise_employee, 'J-Bone Saturno - 50000')
+        self.assertEqual(self.default_raise_employee, 'J-Bone Saturno - 50000')
 
     def test_give_custom_raise(self):
-        new_employee = CreateEmployee('j-bone', 'saturno', 50000)
-        custom_raise_employee = new_employee.give_raise(5000)
-        self.assertEqual(custom_raise_employee, 'J-Bone Saturno - 55000')
+        self.assertEqual(self.custom_raise_employee, 'J-Bone Saturno - 55000')
