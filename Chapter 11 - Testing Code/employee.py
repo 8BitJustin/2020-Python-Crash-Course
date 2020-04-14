3a4c2fe3cc07518edafefd8384211b43cd9c739d
8bitjustin <j.olson.digital@gmail.com>
2020-04-14 10:21:52 -0700

    Employee.py completed.

---
+++ Chapter 11 - Testing Code/employee.py
@@ -0,0 +1,31 @@
+class Employee:
+
+    def __init__(self, first_name, last_name, salary):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def description(self):
+        print(f"First name: {self.first_name.title()}\n"
+              f"Last name: {self.last_name.title()}\n"
+              f"Salary: {int(self.salary)}/yr")
+
+    def give_raise(self, increase=0):
+        if increase:
+            self.salary += increase
+        else:
+            self.salary += 5000
+        return self.salary
+
+
+newbie = Employee("j-bone", "saturno", 75000)
+
+newbie.description()
+
+newbie.give_raise()
+
+newbie.description()
+
+newbie.give_raise(2500)
+
+newbie.description()
