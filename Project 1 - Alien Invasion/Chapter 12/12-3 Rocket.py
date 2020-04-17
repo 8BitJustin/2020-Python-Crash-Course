8c5d459793f3243274a4fa1c5a1f1603067e52f5
8bitjustin <j.olson.digital@gmail.com>
2020-04-17 08:14:33 -0700

    Added game code to 12.3 file.

--- Project 1 - Alien Invasion/Chapter 12/12-3 Rocket.py
+++ Project 1 - Alien Invasion/Chapter 12/12-3 Rocket.py
@@ -1,3 +1,24 @@
 """Make a game that begins with a rocket in the center of the screen. Allow
 the player to move the rocket up, down, left, or right using the four arrow
 keys. Make sure the rocket never moves beyond any edge of the screen."""
+
+import sys
+import pygame
+
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Rocket Game")
+
+    bg_color = (135, 206, 235)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.flip()
+
+
+run_game()
