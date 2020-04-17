6b81385da1829e9f62f7747c98c70f8e2dea7806
8bitjustin <j.olson.digital@gmail.com>
2020-04-17 13:26:53 -0700

    Added movement flags and keys to both tiy_spaceship and 12.3 rocket. 12.3 compeleted.

--- Project 1 - Alien Invasion/Chapter 12/12-3 Rocket.py
+++ Project 1 - Alien Invasion/Chapter 12/12-3 Rocket.py
@@ -17,9 +17,30 @@ def run_game():
     bg_color = (135, 206, 235)
 
     while True:
+        ship.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    ship.moving_left = True
+                elif event.key == pygame.K_UP:
+                    ship.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    ship.moving_down = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    ship.moving_left = False
+                elif event.key == pygame.K_UP:
+                    ship.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    ship.moving_down = False
 
         screen.fill(bg_color)
 
--- Project 1 - Alien Invasion/Chapter 12/tiy_spaceship.py
+++ Project 1 - Alien Invasion/Chapter 12/tiy_spaceship.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Spaceship:
 
     def __init__(self, screen):
@@ -14,6 +15,23 @@ class Spaceship:
         self.rect.centerx = self.screen_rect.centerx
         self.rect.centery = self.screen_rect.centery
 
+        # Movement flag.
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.centerx += 1
+        elif self.moving_left and self.rect.left > 0:
+            self.rect.centerx -= 1
+        elif self.moving_up and self.rect.top > 0:
+            self.rect.centery -= 1
+        elif self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.rect.centery += 1
+
     def blitme(self):
         """Draws ship at current location."""
         self.screen.blit(self.image, self.rect)
+
