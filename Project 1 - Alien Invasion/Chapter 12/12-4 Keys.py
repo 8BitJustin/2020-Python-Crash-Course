2c6d59e7f5bbc71eedb52b0ebef99a89f4b18606
8bitjustin <j.olson.digital@gmail.com>
2020-04-17 13:59:07 -0700

    Completed 12.4.

---
+++ Project 1 - Alien Invasion/Chapter 12/12-4 Keys.py
@@ -0,0 +1,31 @@
+"""Make a Pygame file that creates an empty screen. In the event loop,
+print the event.key attribute whenever a pygame.KEYDOWN event is detected.
+Run program, press various keys to see how Pygame responds."""
+
+import sys
+import pygame
+
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((400, 300))
+    pygame.display.set_caption("Rocket Game")
+
+    bg_color = (135, 206, 235)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                print(event.key)
+                print(event.scancode)
+                print()
+
+        screen.fill(bg_color)
+
+        pygame.display.flip()
+
+
+run_game()
