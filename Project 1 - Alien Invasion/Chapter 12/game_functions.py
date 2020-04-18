bab2b7c711b22a912f0ca7e195c17632eb674e6a
8bitjustin <j.olson.digital@gmail.com>
2020-04-18 15:20:12 -0700

    Altered code that fires bullet within game_function to be it's own function to clean it up.

--- Project 1 - Alien Invasion/Chapter 12/game_functions.py
+++ Project 1 - Alien Invasion/Chapter 12/game_functions.py
@@ -13,9 +13,14 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         # Create a new bullet and add it to the bullets group.
-        if len(bullets) < ai_settings.bullets_allowed:
-            new_bullet = Bullet(ai_settings, screen, ship)
-            bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Fire new bullet if limit not yet reached."""
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
