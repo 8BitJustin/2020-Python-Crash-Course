6a155260f684a8aa561dfea515fe97679c4911db
8bitjustin <j.olson.digital@gmail.com>
2020-04-17 17:55:55 -0700

    Added code to bullet, alien_invasion, and game_functions files. Bullets shoot properly atm.

--- Project 1 - Alien Invasion/Chapter 12/bullet.py
+++ Project 1 - Alien Invasion/Chapter 12/bullet.py
@@ -1,7 +1,8 @@
 import pygame
 from pygame.sprite import Sprite
 
-class Bullet:
+
+class Bullet(Sprite):
     """A class to manage bullets fired from the ship."""
 
     def __init__(self, ai_settings, screen, ship):
