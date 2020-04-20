c7f9321b91e5aa54f4ba84eff082dd4cc6e5b504
8bitjustin <j.olson.digital@gmail.com>
2020-04-20 16:09:12 -0700

    Moved all TIY files from CH12 dir to new TIY directory within CH12.

--- Project 1 - Alien Invasion/Chapter 12/12-5 TIY - Sideways Shooter/tiy_spaceship125.py
+++ Project 1 - Alien Invasion/Chapter 12/12-5 TIY - Sideways Shooter/tiy_spaceship125.py
@@ -8,7 +8,7 @@ class Spaceship:
         self.settings = settings
 
         # Loads ship image and gets it's rect.
-        self.image = pygame.image.load('images/spaceship125-reduced.png')
+        self.image = pygame.image.load('../images/spaceship125-reduced.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
