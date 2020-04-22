f39160ded07046cef9d7807936473ffe323dd2cf
8bitjustin <j.olson.digital@gmail.com>
2020-04-22 15:24:57 -0700

    Added import alien code to alien_invasion.py file.

--- Project 1 - Alien Invasion/Chapter 12/alien_invasion.py
+++ Project 1 - Alien Invasion/Chapter 12/alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 
@@ -19,13 +20,16 @@ def run_game():
     # Make a grp to store bullets in.
     bullets = Group()
 
+    # Make an alien.
+    alien = Alien(ai_settings, screen)
+
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 
 run_game()
