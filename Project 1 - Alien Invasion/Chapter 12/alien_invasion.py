2339025b0ceef8c6e0f2dcab3b84e4000f54702d
8bitjustin <j.olson.digital@gmail.com>
2020-04-23 15:14:26 -0700

    Added group of aliens code to alien_invasion.py.

--- Project 1 - Alien Invasion/Chapter 12/alien_invasion.py
+++ Project 1 - Alien Invasion/Chapter 12/alien_invasion.py
@@ -14,14 +14,13 @@ def run_game():
                                       ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Make a ship.
+    # Make a ship, a group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
-
-    # Make a grp to store bullets in.
     bullets = Group()
+    aliens = Group()
 
-    # Make an alien.
-    alien = Alien(ai_settings, screen)
+    # Create a fleet of aliens.
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # Start the main loop for the game.
     while True:
@@ -29,7 +28,7 @@ def run_game():
         ship.update()
         bullets.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
