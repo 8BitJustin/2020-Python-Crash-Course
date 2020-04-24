da1e3ae1bbc6884f28b2a199cfa51c4d82fca57d
8bitjustin <j.olson.digital@gmail.com>
2020-04-24 09:53:38 -0700

    Changed bullet speed to 3 and ship speed to 4 within settings.

--- Project 1 - Alien Invasion/Chapter 12/settings.py
+++ Project 1 - Alien Invasion/Chapter 12/settings.py
@@ -10,11 +10,11 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings.
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 4
 
         # Bullet Settings.
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = 60, 60, 60
+        self.bullet_color = 153, 204, 255
         self.bullets_allowed = 3
