9a6b23dcfba33f915bd49ead5f7c2e608c8096ca
8bitjustin <j.olson.digital@gmail.com>
2020-04-25 12:34:52 -0700

    Fixed typo in create_fleet method, updated gf.create_fleet within alien_invasion file.

--- Project 1 - Alien Invasion/Chapter 12/game_functions.py
+++ Project 1 - Alien Invasion/Chapter 12/game_functions.py
@@ -91,7 +91,7 @@ def create_fleet(ai_settings, screen, ship, aliens):
 
     # Create first row of aliens.
     for row_number in range(number_rows):
-        for alien_number in rante(number_aliens_x):
+        for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number,
                          row_number)
 
