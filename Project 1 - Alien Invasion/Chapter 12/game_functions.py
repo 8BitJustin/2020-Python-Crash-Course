e25ef000cf0286a098eee58a7f01085c47dac970
8bitjustin <j.olson.digital@gmail.com>
2020-04-25 11:58:44 -0700

    Created get_number_rows method within game_functions.

--- Project 1 - Alien Invasion/Chapter 12/game_functions.py
+++ Project 1 - Alien Invasion/Chapter 12/game_functions.py
@@ -62,6 +62,14 @@ def get_number_aliens_x(ai_settings, alien_width):
     return number_aliens_x
 
 
+def get_number_rows(ai_settings, ship_height, alien_height):
+    """Determine number of rows of aliens that fit on the screen."""
+    available_space_y = (ai_settings.screen_height -
+                         (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
+
+
 def create_alien(ai_settings, screen, aliens, alien_number):
     """Create an alien and place it in the row."""
     alien = Alien(ai_settings, screen)
