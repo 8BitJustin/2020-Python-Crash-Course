02a142796f91405f20d7069027153e689d9d0587
8bitjustin <j.olson.digital@gmail.com>
2020-04-27 16:35:13 -0700

    Playing with axis ranges and edgecolors.

--- Project 2 - Data Visualization/Chapter 15 - Generating Data/scatter_squares.py
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/scatter_squares.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
-x_values = list(range(1, 1000))
+x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s=40)
+plt.scatter(x_values, y_values, edgecolors='none', s=100)
 
 
 # Set chart title and label axes.
@@ -13,5 +13,8 @@ plt.ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
 plt.tick_params(axis="both", which="major", labelsize=14)
+
+# Set the range for each axis.
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
