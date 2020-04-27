6c7a64380ca7365bd7f1438741587831ade8f218
8bitjustin <j.olson.digital@gmail.com>
2020-04-27 16:39:48 -0700

    Playing with Colormap.

--- Project 2 - Data Visualization/Chapter 15 - Generating Data/scatter_squares.py
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/scatter_squares.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, edgecolors='none', s=100)
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds,
+            edgecolors='none', s=10)
 
 
 # Set chart title and label axes.
