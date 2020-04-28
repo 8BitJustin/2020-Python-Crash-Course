6d29354118c7c5e0893011efcce1a1151b640b6c
8bitjustin <j.olson.digital@gmail.com>
2020-04-28 10:57:50 -0700

    Completed 15.1.

--- Project 2 - Data Visualization/Chapter 15 - Generating Data/15-1 Cubes.py
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/15-1 Cubes.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4, 5]
-y = [1, 9, 27, 81, 243]
-plt.plot(x, y, linewidth=3)
+x_values = list(range(1, 5001))
+y_values = [x ** 3 for x in x_values]
+plt.plot(x_values, y_values, linewidth=3)
 
 plt.title('Cubed', fontsize=24)
 plt.xlabel("Value", fontsize=12)
-plt.ylabel("Qubed Value", fontsize=12)
+plt.ylabel("Cubed Value", fontsize=12)
 
 plt.tick_params(axis='both', labelsize=14)
 
