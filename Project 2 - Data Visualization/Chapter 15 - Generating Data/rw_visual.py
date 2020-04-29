c9f9a7660e9a33900ddf8ed0117093bbcdfe7887
8bitjustin <j.olson.digital@gmail.com>
2020-04-29 11:36:17 -0700

    Added while loop to rw_visual, to make new random walk upon request.

--- Project 2 - Data Visualization/Chapter 15 - Generating Data/rw_visual.py
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/rw_visual.py
@@ -2,9 +2,14 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make random walk, and plot points.
-rw = RandomWalk()
-rw.fill_walk()
+# Keep making random walks as long as program is active.
+while True:
+    # Make random walk, and plot points.
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
