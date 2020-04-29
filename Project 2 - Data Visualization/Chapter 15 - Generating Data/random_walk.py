e5a4cb3ec97520a88df061b57b477ee09f76c5f2
8bitjustin <j.olson.digital@gmail.com>
2020-04-29 10:51:35 -0700

    Corrected random_walk file, added 5000 to num_points parameter, as argument was missing.

--- Project 2 - Data Visualization/Chapter 15 - Generating Data/random_walk.py
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/random_walk.py
@@ -4,7 +4,7 @@ from random import choice
 class RandomWalk:
     """A class to generate random walk."""
 
-    def __init__(self, num_points):
+    def __init__(self, num_points=5000):
         """Initialize attributes of a walk."""
         self.num_points = num_points
 
