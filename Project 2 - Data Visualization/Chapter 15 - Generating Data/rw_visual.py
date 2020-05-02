6fba16346c1806c6e8bbba80bddfc3cb6a082c85
8bitjustin <j.olson.digital@gmail.com>
2020-05-01 21:55:00 -0700

    Added code to remove axes.

--- Project 2 - Data Visualization/Chapter 15 - Generating Data/rw_visual.py
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/rw_visual.py
@@ -12,9 +12,14 @@ while True:
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
                 cmap=plt.cm.Blues, edgecolors='none', s=15)
 
+    # Emphasize the first and last points.
     plt.scatter(0, 0, c="green", edgecolors="none", s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c="red",
                 edgecolors="none", s=100)
+
+    # Remove the axes.
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
 
     plt.show()
 
