7c79bd8a3d7684365ef71d3eccffcbc55ddec799
8bitjustin <j.olson.digital@gmail.com>
2020-05-03 11:50:36 -0700

    Completed 15.3.

--- Project 2 - Data Visualization/Chapter 15 - Generating Data/15-3 Molecular_Motion.py
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/15-3 Molecular_Motion.py
@@ -9,16 +9,15 @@ while True:
     rw.fill_walk()
 
     # Set the size of the plotting window.
-    plt.figure(dpi=128, figsize=(15, 8))
+    plt.figure(dpi=128, figsize=(10, 6))
 
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers,
-                cmap=plt.cm.Blues, edgecolors='none', s=1)
+    plt.plot(rw.x_values, rw.y_values)
 
     # Emphasize the first and last points.
-    plt.scatter(0, 0, c="green", edgecolors="none", s=100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c="red",
-                edgecolors="none", s=100)
+    # plt.scatter(0, 0, c="green", edgecolors="none", s=100)
+    # plt.scatter(rw.x_values[-1], rw.y_values[-1], c="red",
+    #             edgecolors="none", s=100)
 
     # Remove the axes.
     plt.axes().get_xaxis().set_visible(False)
