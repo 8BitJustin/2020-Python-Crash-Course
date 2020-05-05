7ab645058b19be57555b9b970ed867d6ba807f75
8bitjustin <j.olson.digital@gmail.com>
2020-05-05 10:04:17 -0700

    Added instruction to 15.5.

---
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/15-5 Refactoring.py
@@ -0,0 +1,8 @@
+"""
+The method fill_walk() is lengthy. Create a new method called get_step() to
+determine the direction and distance for each step, and then calculate the
+step. You should end up with two calls to get_step() in fill_walk():
+
+x_step = get_step()
+y_step = get_step()
+"""
