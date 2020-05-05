0d74b61e9a0e46abe04795e1cdc1c51d85cf0fbc
8bitjustin <j.olson.digital@gmail.com>
2020-05-05 16:07:40 -0700

    Added code to analyze the results of side is rolled out of 1000 rolls.

--- Project 2 - Data Visualization/Chapter 15 - Generating Data/Pygal/die_visual.py
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/Pygal/die_visual.py
@@ -5,8 +5,15 @@ die = Die()
 
 # Make some rolls, store results in a list.
 results = []
-for roll_num in range(100):
+
+for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# Analyze results.
+frequencies = []
+for value in range(1, die.sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
