a2885e171c74620b81d8eb380e20d1c76dc4df76
8bitjustin <j.olson.digital@gmail.com>
2020-05-05 17:18:31 -0700

    Adjusted code for two die to be rolled, and analyzed with new .svg file, two_die_visual.

--- Project 2 - Data Visualization/Chapter 15 - Generating Data/Pygal/die_visual.py
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/Pygal/die_visual.py
@@ -2,27 +2,29 @@ from die import Die
 import pygal
 
 # Create D6.
-die = Die()
+die_1 = Die()
+die_2 = Die()
 
 # Make some rolls, store results in a list.
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analyze results.
 frequencies = []
-for value in range(1, die.sides+1):
+max_result = die_1.sides + die_2.sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize results.
 hist = pygal.Bar()
 
-hist.title = "Results of rolling a D6 1000 times."
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.title = "Results of rolling two D6 die 1000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add("D6", frequencies)
-hist.render_to_file('die_visual.svg')
+hist.add("D6 + D6", frequencies)
+hist.render_to_file('two_die_visual.svg')
