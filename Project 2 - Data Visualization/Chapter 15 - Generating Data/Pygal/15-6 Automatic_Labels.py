7c437c04f212e46c2ca4261dcaefaaee50a0f31b
8bitjustin <j.olson.digital@gmail.com>
2020-05-06 11:43:21 -0700

    Created 15.6.

---
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/Pygal/15-6 Automatic_Labels.py
@@ -0,0 +1,6 @@
+"""
+Modify die.py and dice_visual.py by replacing the list we used to set the
+value of hist.x_labels with a loop to generate this list automatically. If
+you're comfortable with list comprehensions, try replacing the other for
+loops in die_visual.py and dice_visual.py with comprehensions as well.
+"""
