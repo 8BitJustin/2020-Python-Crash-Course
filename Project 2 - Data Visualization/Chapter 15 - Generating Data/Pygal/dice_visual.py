90a24edfe81b29d803d483847902ceec50141071
8bitjustin <j.olson.digital@gmail.com>
2020-05-06 11:07:57 -0700

    Added histogram code to dice_visual.py file. Will save dice_visual.svg file any time it's ran. Also removed older .svg files.

---
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/Pygal/dice_visual.py
@@ -0,0 +1,30 @@
+import pygal
+from die import Die
+
+# Create a two D6.
+die_1 = Die()
+die_2 = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# Analyze results.
+frequencies = []
+max_result = die_1.sides + die_2.sides
+for value in range(2, max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize results.
+hist = pygal.Bar()
+
+hist.title = "Results of rolling two D6 1000 times."
+hist.x_labels = list(range(2, 13))
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add("D6 + D6", frequencies)
+hist.render_to_file('dice_visual.svg')
