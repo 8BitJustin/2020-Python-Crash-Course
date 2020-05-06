038082cfdaa5f34e3f16d3773648bd39cc8ddc95
8bitjustin <j.olson.digital@gmail.com>
2020-05-05 17:50:25 -0700

    Adjusted hist.x_labels within die_visual. Variable originally takes a list of numbers to go along the x axis. To keep from having to change these manually, I input a range that can be altered easier.

--- Project 2 - Data Visualization/Chapter 15 - Generating Data/Pygal/die_visual.py
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/Pygal/die_visual.py
@@ -22,7 +22,8 @@ for value in range(2, max_result+1):
 hist = pygal.Bar()
 
 hist.title = "Results of rolling two D6 die 1000 times."
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+# hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.x_labels = list(range(2, 13))
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
