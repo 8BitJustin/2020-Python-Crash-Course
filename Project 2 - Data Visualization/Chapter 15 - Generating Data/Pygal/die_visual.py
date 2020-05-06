d948bb79f9db3e9cd4c6fa08d23e874e35a44e5f
8bitjustin <j.olson.digital@gmail.com>
2020-05-06 10:48:50 -0700

    Added histogram code to die_visual.py file. Will save die_visual.svg file any time it's ran.

--- Project 2 - Data Visualization/Chapter 15 - Generating Data/Pygal/die_visual.py
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/Pygal/die_visual.py
@@ -1,3 +1,4 @@
+import pygal
 from die import Die
 
 # Create a D6.
@@ -15,4 +16,13 @@ for value in range(1, die.roll()+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+# Visualize results.
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = list(range(1, 7))
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add("D6", frequencies)
+hist.render_to_file('die_visual.svg')
