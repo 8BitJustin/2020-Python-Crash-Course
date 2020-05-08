69a2b8f06c397cefa0f8f12723d99ef8347f059c
8bitjustin <j.olson.digital@gmail.com>
2020-05-08 15:18:22 -0700

    Adjusted highs_lows file. Now prints the index and column header.

--- Project 2 - Data Visualization/Chapter 16 - Downloading Data/highs_lows.py
+++ Project 2 - Data Visualization/Chapter 16 - Downloading Data/highs_lows.py
@@ -4,4 +4,6 @@ filename = 'sitka_weather_07-2014.csv'
 with open(filename) as file:
     reader = csv.reader(file)
     header_row = next(reader)
-    print(header_row)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
