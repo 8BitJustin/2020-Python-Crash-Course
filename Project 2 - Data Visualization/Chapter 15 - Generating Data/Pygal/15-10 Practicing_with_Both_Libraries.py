4438f0e0784566e3bb47a9546bb20b21db213d75
8bitjustin <j.olson.digital@gmail.com>
2020-05-08 08:41:32 -0700

    Disregard, 15.10 completed.

--- Project 2 - Data Visualization/Chapter 15 - Generating Data/Pygal/15-10 Practicing_with_Both_Libraries.py
+++ Project 2 - Data Visualization/Chapter 15 - Generating Data/Pygal/15-10 Practicing_with_Both_Libraries.py
@@ -3,15 +3,12 @@ from die import Die
 
 die_1 = Die()
 
-results = [die_1.roll for roll in range(100)]
-
-x_values = list(range(1, 7))
-y_values = results
-
-plt.scatter(x_values, y_values, s=100)
-
+results = [die_1.roll() for roll in range(100)]
+plt.figure(figsize=(12, 4))
 plt.title("6-sided rolls")
-plt.xlabel("Side")
-plt.ylabel("Results")
+plt.xlabel("Roll")
+plt.ylabel("Side")
+
+plt.plot(results, linewidth=4)
 
 plt.show()
