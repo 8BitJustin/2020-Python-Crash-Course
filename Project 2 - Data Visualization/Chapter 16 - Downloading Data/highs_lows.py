d854d4d95a1fc590221b035596511c29e4356801
8bitjustin <j.olson.digital@gmail.com>
2020-05-08 22:00:45 -0700

    Added code to append high temp as int to list.

--- Project 2 - Data Visualization/Chapter 16 - Downloading Data/highs_lows.py
+++ Project 2 - Data Visualization/Chapter 16 - Downloading Data/highs_lows.py
@@ -1,9 +1,14 @@
 import csv
 
+# Get high temps from file
 filename = 'sitka_weather_07-2014.csv'
 with open(filename) as file:
     reader = csv.reader(file)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    highs = []
+    for row in reader:
+        high = int(row[1])
+        highs.append(high)
+
+    print(highs)
