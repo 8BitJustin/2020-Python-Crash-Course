525dbc3756b4441518e62dc1b616f5f02d1f8463
8bitjustin <j.olson.digital@gmail.com>
2020-05-11 07:30:50 -0700

    Country_codes added to world_population file.

--- Project 2 - Data Visualization/Chapter 16 - Downloading Data/world_population.py
+++ Project 2 - Data Visualization/Chapter 16 - Downloading Data/world_population.py
@@ -1,5 +1,6 @@
 """Using data from 'population_data.json' file."""
 import json
+from country_codes import get_country_code
 
 # Load the data into a list.
 filename = 'population_data.json'
@@ -11,4 +12,8 @@ for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
-        print(f"{country_name}: {population}")
+        code = get_country_code(country_name)
+        if code:
+            print(f"{code}: {str(population)}")
+        else:
+            print(f"ERROR - {country_name}")
