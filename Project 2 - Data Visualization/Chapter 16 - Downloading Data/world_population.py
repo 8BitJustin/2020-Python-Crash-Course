c32e7d81695e7e38b66fd62e15ce333764bea36a
8bitjustin <j.olson.digital@gmail.com>
2020-05-11 07:06:24 -0700

    Added basic code to load and print population data.

---
+++ Project 2 - Data Visualization/Chapter 16 - Downloading Data/world_population.py
@@ -0,0 +1,14 @@
+"""Using data from 'population_data.json' file."""
+import json
+
+# Load the data into a list.
+filename = 'population_data.json'
+with open(filename) as f:
+    pop_data = json.load(f)
+
+# Print 2010 population for each country.
+for pop_dict in pop_data:
+    if pop_dict['Year'] == '2010':
+        country_name = pop_dict['Country Name']
+        population = pop_dict['Value']
+        print(f"{country_name}: {population}")
