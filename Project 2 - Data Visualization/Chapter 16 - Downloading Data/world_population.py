2e21bc13aa071054d92e2730315e203b98b07a96
8bitjustin <j.olson.digital@gmail.com>
2020-05-12 07:52:16 -0700

    Added pygal world code to world_population file. Created world_population.svg when ran.

--- Project 2 - Data Visualization/Chapter 16 - Downloading Data/world_population.py
+++ Project 2 - Data Visualization/Chapter 16 - Downloading Data/world_population.py
@@ -1,11 +1,16 @@
 """Using data from 'population_data.json' file."""
 import json
+from pygal.maps.world import World
+
 from country_codes import get_country_code
 
 # Load the data into a list.
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
+
+# Build a dictionary of population data.
+cc_populations = {}
 
 # Print 2010 population for each country.
 for pop_dict in pop_data:
@@ -14,6 +19,10 @@ for pop_dict in pop_data:
         population = int(float(pop_dict['Value']))
         code = get_country_code(country_name)
         if code:
-            print(f"{code}: {str(population)}")
-        else:
-            print(f"ERROR - {country_name}")
+            cc_populations[code] = population
+
+wm = World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
