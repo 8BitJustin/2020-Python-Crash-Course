3826274ba5a353a8dca3624dbd3cbfa4dd5d1ede
8bitjustin <j.olson.digital@gmail.com>
2020-05-16 11:52:47 -0700

    Added comments and small corrections to 16.6.

--- Project 2 - Data Visualization/Chapter 16 - Downloading Data/16-6 Gross_Domestic_Product.py
+++ Project 2 - Data Visualization/Chapter 16 - Downloading Data/16-6 Gross_Domestic_Product.py
@@ -2,10 +2,12 @@ import json
 from pygal.maps.world import World
 from country_codes import get_country_code
 
+# Loads data into a list.
 filename = 'gpd.json'
 with open(filename) as f:
     gdp_data = json.load(f)
 
+# Build a dictionary of GDP data.
 values = {}
 for gdp_dict in gdp_data:
     if gdp_dict['Year'] == 2016:
@@ -18,21 +20,22 @@ for gdp_dict in gdp_data:
 # Value levels.
 cc_value_1, cc_value_2, cc_value_3 = {}, {}, {}
 for c, v in values.items():
-    if v < 10000000000:
+    if v < 100000000000:
         cc_value_1[c] = v
     elif v < 1000000000000:
         cc_value_2[c] = v
     else:
         cc_value_3[c] = v
 
+# Prints numerical value of the three categories of GDP.
 print(len(cc_value_1), len(cc_value_2), len(cc_value_3))
 
 wm = World()
 wm.title = "World Gross Domestic Product in 2016, by country"
 # Keeps value from being scientific notation.
 wm.value_formatter = lambda x: "{:,}".format(x)
-wm.add('0-10b', cc_value_1)
-wm.add('10b-1t', cc_value_2)
-wm.add('>1t', cc_value_3)
+wm.add('< 100B', cc_value_1)
+wm.add('100B - 1T', cc_value_2)
+wm.add('> 1T', cc_value_3)
 
 wm.render_to_file('world_gdp_2016.svg')
