0c63b522dc93f384da958ec5dec6d0aa7d8b4909
8bitjustin <j.olson.digital@gmail.com>
2020-05-18 09:06:15 -0700

    Added pygal code to file. Creates and updates python_repos.svg file when ran.

--- Project 2 - Data Visualization/Chapter 17 - Working With APIs/python_repos.py
+++ Project 2 - Data Visualization/Chapter 17 - Working With APIs/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -13,12 +15,17 @@ print(f"Total Python repositories: {response_dict['total_count']}")
 
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
 
-print(f"\nSelected information about each repository:")
+names, stars = [], []
 for repo_dict in repo_dicts:
-    print(f"\nName: {repo_dict['name']}")
-    print(f"Owner: {repo_dict['owner'] ['login']}")
-    print(f"Stars: {repo_dict['stargazers_count']}")
-    print(f"Repository: {repo_dict['html_url']}")
-    print(f"Description: {repo_dict['description']}")
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# Create visualization.
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on Github'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
