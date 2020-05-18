479f243b121090a8d7a01de98e782a5fa3a1a522
8bitjustin <j.olson.digital@gmail.com>
2020-05-18 07:05:02 -0700

    Added print statement if status code == 200.

--- Project 2 - Data Visualization/Chapter 17 - Working With APIs/python_repos.py
+++ Project 2 - Data Visualization/Chapter 17 - Working With APIs/python_repos.py
@@ -4,6 +4,8 @@ import requests
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 print("Status code: ", r.status_code)
+if r.status_code == 200:
+    print('Success.')
 
 # Store API response in a variable.
 response_dict = r.json()
