5da7dc1ebea55c32fb103b44cb10350e338916cc
8bitjustin <j.olson.digital@gmail.com>
2020-05-18 13:31:33 -0700

    Added my_config, as it will make passing into .Bar() easier. Also imported NeonStyle.

--- Project 2 - Data Visualization/Chapter 17 - Working With APIs/python_repos.py
+++ Project 2 - Data Visualization/Chapter 17 - Working With APIs/python_repos.py
@@ -1,6 +1,7 @@
 import requests
 import pygal
-from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS, \
+    NeonStyle as NS
 
 # Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -21,9 +22,20 @@ for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
 
-# Create visualization.
-my_style = LS('#228b22', base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+# Create visualization. Commented out to utilize NeonStyle.
+# my_style = LS('#228b22', base_style=LCS)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=NS)
 chart.title = 'Most-Starred Python Projects on Github'
 chart.x_labels = names
 
