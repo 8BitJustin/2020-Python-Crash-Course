06936f095be91129a6fed109ad0f13ecb2d91fcf
8bitjustin <j.olson.digital@gmail.com>
2020-05-18 17:20:12 -0700

    Added descriptions to labels for bar graph.

--- Project 2 - Data Visualization/Chapter 17 - Working With APIs/python_repos.py
+++ Project 2 - Data Visualization/Chapter 17 - Working With APIs/python_repos.py
@@ -1,7 +1,6 @@
 import requests
 import pygal
-from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS, \
-    NeonStyle as NS
+from pygal.style import NeonStyle as NS
 
 # Make an API call and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -17,10 +16,15 @@ print(f"Total Python repositories: {response_dict['total_count']}")
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': str(repo_dict['description']),
+        }
+    plot_dicts.append(plot_dict)
 
 # Create visualization. Commented out to utilize NeonStyle.
 # my_style = LS('#228b22', base_style=LCS)
@@ -39,5 +43,5 @@ chart = pygal.Bar(my_config, style=NS)
 chart.title = 'Most-Starred Python Projects on Github'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
