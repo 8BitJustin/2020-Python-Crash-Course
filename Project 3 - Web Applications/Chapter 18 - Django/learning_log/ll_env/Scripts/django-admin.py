5fdf48e0d3bf62dce5408bbf984848d8ac343530
8bitjustin <j.olson.digital@gmail.com>
2020-05-27 08:02:29 -0700

    All files added.

---
+++ Project 3 - Web Applications/Chapter 18 - Django/learning_log/ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\python\learning\books\python crash course 2020\project 3 - web applications\chapter 18 - django\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
